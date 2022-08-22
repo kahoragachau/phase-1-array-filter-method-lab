c12cbcbc1e941cb0cf6c22a626ee5cca622398e1
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-22 22:30:42 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers,names){
+    let filtering = drivers.filter(name => name.toUpperCase() === names.toUpperCase() )
+    return filtering
+}
+function fuzzyMatch(drivers, letters){
+    let filtering = drivers.filter(letter => letter[0] === letters[0])
+    return filtering
+}
+
+function matchName(drivers, person){
+    // console.log(drivers[0]["name"])}
+    let filtering = drivers.filter(drivers => drivers.name === person)
+    console.log(filtering)
+    return filtering
+}
